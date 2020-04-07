cadad3f7d0cc0a3301a75a6c4925b274c1bd9062
al1fake <55156410+al1fake@users.noreply.github.com>
2020-04-07 14:28:12 +0300

    Lab3

---
+++ C#Labs/Lab3/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3rdCsharp
+{
+    class Human
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        protected string ID { get; set; }
+
+        public Human()
+        {
+            Name = "Ilya";
+            Age = 18;
+            ID = GenerateID();
+        }
+
+        public Human(string n = " ", int a = 0)
+        {
+            Name = n;
+            Age = a;
+            ID = GenerateID();
+        }
+
+        protected static string GenerateID()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+
+        public override string ToString()
+        {
+            return ($"Name: {Name}\nAge: {Age}\nID: {ID}\n");
+        }
+    }
+}
+
---
+++ C#Labs/Lab3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _3rdCsharp
+{
+    class Program
+    {
+        static void SetArr(Human[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("\n");
+                arr[i] = new Human();
+                Console.Write("Input Name: ");
+                arr[i].Name = Console.ReadLine();
+                Console.Write("Input Age: ");
+                arr[i].Age = CheckInt();
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + arr[i]);
+            }
+        }
+
+        static int CheckInt()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                Console.Write("Incorrect input. Try again: ");
+            }
+            return a;
+        }
+
+        static void Main()
+        {
+            Human obj1 = new Human();
+            Human obj2 = new Human("Вася", 18);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+
+            Human[] arr = new Human[2];
+            SetArr(arr);
+            Console.WriteLine();
+        }
+    }
+}
+
+
