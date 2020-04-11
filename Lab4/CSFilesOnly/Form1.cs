c6f9fffc500c59ea594718995c5a4e2a31304c0a
al1fake <55156410+al1fake@users.noreply.github.com>
2020-04-11 15:08:30 +0300

    added CS Files Folder

---
+++ Lab4/CSFilesOnly/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4CHARP
+{
+    public partial class Form1 : Form
+    {
+        private Player _player;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog dlgOpen = new OpenFileDialog())
+            {
+                dlgOpen.Filter = "Mp3 File|*.mp3";
+                dlgOpen.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+                if(dlgOpen.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    _player = new Player(dlgOpen.FileName);
+                }
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if (_player != null)
+            _player.Play();
+        }
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            if (_player != null)
+                _player.Dispose();
+        }
+    }
+}
+    
---
+++ Lab4/CSFilesOnly/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4CHARP
+{
+    class Player : IDisposable 
+    {
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+        public Player(string fileName)
+        {
+            const string FORMAT = @"open ""{0}"" type mpegvideo alias MediaFile";
+            string command = String.Format(FORMAT, fileName);
+            mciSendString(command, null, 0, IntPtr.Zero);
+        }
+        public void Play()
+        {
+            string command = "play MediaFile";
+            mciSendString(command, null, 0, IntPtr.Zero);
+        }
+        public void Stop()
+        {
+            string command = "stop MediaFile";
+            mciSendString(command, null, 0, IntPtr.Zero);
+        }
+
+        
+
+
+        public void Dispose()
+        {
+            string command = "close MediaFile";
+            mciSendString(command, null, 0, IntPtr.Zero);
+        }
+    }
+}
