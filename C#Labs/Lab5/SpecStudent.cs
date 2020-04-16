65d9369abc03cbe033fea87dac37bf4289e66ef8
al1fake <55156410+al1fake@users.noreply.github.com>
2020-04-16 21:03:19 +0300

    Lab5

---
+++ C#Labs/Lab5/SpecStudent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3rdCsharp
+{
+    class SpecStudent :Student
+    {
+        string specName;
+
+        public struct Exams
+        {
+            public string subject;
+            public DateTime date;
+        }
+        public Exams exams;
+        public courseNum courseNum;
+        public SpecStudent(courseNum z,string n = "N/A", int a = 0, string k = "N/A", string l = "N/A", DateTime m = default)
+        {
+            name = n;
+            age = a;
+            courseNum = z;
+            specName = k;
+            exams.subject = l;
+            exams.date = m;
+            ID = GenerateID();
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Name:{0}",name);
+            Console.WriteLine("Age:{0}", age);
+            Console.WriteLine("Course:{0}", courseNum);
+            Console.WriteLine("ID:{0}", ID);
+            Console.WriteLine("Speciality Name:{0}", specName);
+            Console.WriteLine("Examination:{0}", exams.subject);
+            Console.WriteLine("Date:{0}", exams.date);
+        
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ C#Labs/Lab5/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3rdCsharp
+{
+    enum courseNum
+    {
+        First = 1,
+        Second,
+        Third,
+        Fourth,
+        Fivth
+    }
+    class Student: Human
+    {
+       
+        public courseNum _courseNum;
+        public Student()
+        {
+            name = "Ilya";
+            age = 18;
+            ID = GenerateID();
+        }
+        public Student(string n, int a, courseNum z)
+        {
+            name = n;
+            age = a;
+            _courseNum = z;
+            ID = GenerateID();
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Name:{0}", name);
+            Console.WriteLine("Age:{0}", age);
+            Console.WriteLine("Course:{0}", _courseNum);
+            Console.WriteLine("ID:{0}", ID);
+            Console.WriteLine("");
+        }
+
+    }
+    
+
+    
+}
