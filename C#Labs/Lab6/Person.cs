dbd0ad1fb6f14e4934c97755a63c4cc41decece0
al1fake <55156410+al1fake@users.noreply.github.com>
2020-04-17 14:50:28 +0300

    Lab6

---
+++ C#Labs/Lab6/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3rdCsharp
+{
+    class Human
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string hobbie { get; set; }
+        protected string ID { get; set; }
+
+        public Human()
+        {
+            name = "Ilya";
+            age = 18;
+            ID = GenerateID();
+        }
+
+        public Human(string n = " ", int a = 0)
+        {
+            name = n;
+            age = a;
+            ID = GenerateID();
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine("Name:{0}", name);
+            Console.WriteLine("Age:{0}", age);
+            Console.WriteLine("ID:{0}", ID);
+            Console.WriteLine("");
+        }
+
+
+
+
+
+
+        protected static string GenerateID()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+
+        public override string ToString()
+        {
+            return ($"Name: {name}\nAge: {age}\nID: {ID}\n");
+        }
+    }
+}
+
---
+++ C#Labs/Lab6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _3rdCsharp
+{
+    class Program
+    {
+       
+        static int CheckInt()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                Console.Write("Incorrect input. Try again: ");
+            }
+            return a;
+        }
+
+        static void Main()
+        {
+
+          
+            SpecStudent specStudent = new SpecStudent((courseNum)1,(SpecialtyName)1,"Вася", 18,"MGIA",new DateTime(2020,12,30, 17,30,00),"Sport gamess");
+            specStudent.ShowInfo();
+   
+        }
+    }
+}
+
+
