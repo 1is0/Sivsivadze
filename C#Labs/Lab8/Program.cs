3f6ed8f5283a24e6837dfde29a003b4f5bd890ec
al1fake <55156410+al1fake@users.noreply.github.com>
2020-04-18 15:32:39 +0300

    Lab8

---
+++ C#Labs/Lab8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace _3rdCsharp
+{
+    class Program
+    {
+       
+        delegate string NameShow(string _1,string _2);
+       
+        static int CheckInt()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                Console.Write("Incorrect input. Try again: ");
+            }
+            return a;
+        }
+
+        static void Main()
+        {
+            
+            NameShow nameShow = (x, y) => x + y;
+            SpecStudent specStudent = new SpecStudent((courseNum)1,(SpecialtyName)1,"Vasya", 16,"MGIA",new DateTime(2020,12,30, 17,30,00),"Sport games");
+            Console.WriteLine(nameShow("Name:", specStudent.name));
+            specStudent._123123 += DisplayMessage;
+            if (specStudent.age<17)
+            {
+                Console.WriteLine("Age:N/A");
+            }
+            else
+            {
+                specStudent.showAge();
+            }
+
+            specStudent.ShowInfo();
+
+            static void DisplayMessage(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
+
+
---
+++ C#Labs/Lab8/SpecStudent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3rdCsharp
+{
+    class SpecStudent :Student, Interface1
+    {
+        
+        public struct Exams
+        {
+            public string subject;
+            public DateTime date;
+        }
+        public Exams exams;
+        public courseNum courseNum;
+        public SpecialtyName specialtyName;
+        public SpecStudent(courseNum z,SpecialtyName k,string n = "N/A", int a = 0, string l = "N/A", DateTime m = default, string b = "N/A")
+        {
+            name = n;
+            age = a;
+            courseNum = z;
+            specialtyName = k;
+            exams.subject = l;
+            exams.date = m;
+            hobbie = b;
+            ID = GenerateID();
+        }
+        public delegate void lr8event(string message);
+        public event lr8event _123123;
+        
+        public void showAge()
+        {
+            _123123?.Invoke($"Age:{age}");
+        }
+        public override void ShowInfo()
+        {
+            
+            Console.WriteLine("Course:{0}", courseNum);
+            Console.WriteLine("ID:{0}", ID);
+            Console.WriteLine("Speciality Name:{0}", specialtyName);
+            hobbieName();
+            Console.WriteLine("Examination:{0}", exams.subject);
+            Console.WriteLine("Date:{0}", exams.date);
+        
+            Console.WriteLine("");
+        }
+        public void hobbieName()
+        {
+            Console.WriteLine("Hobbie:{0}", hobbie);
+        }
+    }
+}
