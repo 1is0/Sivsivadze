7474d373f9d50bf01bbcae3a427d87ad9e06b2e4
al1fake <55156410+al1fake@users.noreply.github.com>
2020-04-23 17:24:11 +0300

    Lab7

---
+++ C#Labs/Lab7/NewInteger.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7
+{
+    class NewInteger
+    {
+        public int Value { get; set; }
+
+        public static NewInteger operator +(NewInteger c1, NewInteger c2)
+        {
+            return new NewInteger { Value = c1.Value + c2.Value };
+        }
+
+        public static NewInteger operator *(NewInteger c1, NewInteger c2)
+        {
+            return new NewInteger { Value = c1.Value * c2.Value };
+        }
+
+        public static NewInteger operator -(NewInteger c1, NewInteger c2)
+        {
+            return new NewInteger { Value = c1.Value - c2.Value };
+        }
+
+        public static NewInteger operator /(NewInteger c1, NewInteger c2)
+        {
+            return new NewInteger { Value = c1.Value / c2.Value };
+        }
+
+        public static NewInteger operator %(NewInteger c1, NewInteger c2)
+        {
+            return new NewInteger { Value = c1.Value % c2.Value };
+        }
+
+
+
+
+
+        public static bool operator >(NewInteger c1, NewInteger c2)
+        {
+            return c1.Value > c2.Value;
+        }
+        public static bool operator <(NewInteger c1, NewInteger c2)
+        {
+            return c1.Value < c2.Value;
+        }
+        public static bool operator ==(NewInteger c1, NewInteger c2)
+        {
+            return c1.Value == c2.Value;
+        }
+        public static bool operator !=(NewInteger c1, NewInteger c2)
+        {
+            return c1.Value != c2.Value ;
+        }
+
+
+        public static int IntChecker()
+        {
+            int a;
+            while (!Int32.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Wrong input.Try again: ");
+            }
+            return a;
+        }
+
+
+        public static int NatChecker()
+        {
+            int a;
+            while ((!Int32.TryParse(Console.ReadLine(), out a)) || a <= 0)
+            {
+                Console.WriteLine("Wrong input.Try again: ");
+            }
+            return a;
+        }
+
+        public static string StrMethod1(int a,string prev)
+        {
+            string b;
+            b = prev + $" = {a}";
+
+            return b;
+        }
+        /*
+        public static string StrMethod2(int a, string prev)
+        {
+            string b, c;
+            c = ;
+
+            return b;
+        }
+        */
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ C#Labs/Lab7/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            Console.WriteLine("Input integer number(N): ");
+            n = NewInteger.IntChecker();
+
+            Console.WriteLine("Input integer number > 0 (M): ");
+            m = NewInteger.NatChecker();
+          
+            Console.WriteLine(NewInteger.StrMethod1(n, "N"));
+            Console.WriteLine("M = {0}", m);
+
+            NewInteger num1 = new NewInteger { Value = n };
+            NewInteger num2 = new NewInteger { Value = m };
+
+            bool check = num1 == num2;
+            switch(check)
+            {
+                case false:
+                    {
+                        check = num1 > num2;
+                       switch(check)
+                        {
+                            case false:
+                                {
+                                    Console.WriteLine("N < M");
+                                    break;
+                                }
+                            case true:
+                                {
+                                    Console.WriteLine("N > M");
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case true:
+                    {
+                        Console.WriteLine("N = M");
+                        break;
+                    }
+            }
+            Console.WriteLine("N + M = {0}", (num1 + num2).Value);
+
+            Console.WriteLine("N - M = {0}", (num1 - num2).Value);
+
+            Console.WriteLine("N * M = {0}", (num1 * num2).Value);
+
+            Console.WriteLine("N / M = {0}", (num1 / num2).Value);
+
+            Console.WriteLine("N % M = {0}", (num1 % num2).Value);
+
+            Console.WriteLine("M % N = {0}", (num2 % num1).Value);
+
+
+
+        }
+    }
+}
